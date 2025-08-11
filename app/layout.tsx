45de9714a153686b5eacbb3ddbf04e5fa8dda7e6
ahmed <ahmeddecodesign@gmail.com>
2025-08-11 20:26:23 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import localFont from "next/font/local"
 import Header from "@/components/layout/Header"
 import Footer from "@/components/layout/Footer"
 import { roboto, robotoSerif } from "@/fonts"
