b31c4f9129edfef0ddd6b64d3cf5f058e24bbe1c
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-02-19 20:38:44 +0200

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -3,16 +3,16 @@ import { createI18nMiddleware } from 'next-international/middleware'
 import { NextRequest } from 'next/server'
 
 const I18nMiddleware = createI18nMiddleware({
-  locales: [ 'en', 'ar' ],
+  locales: ['en', 'ar'],
   defaultLocale: 'en',
 })
-export { auth as Middleware } from "./auth"
+export { auth as Proxy } from "./auth"
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return I18nMiddleware(request)
 }
 
 export const config = {
-  matcher: [ '/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)' ],
+  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)'],
 }
 
